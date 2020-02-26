381220eac400a410552e16a67d50325da25da47d
HKiwooma <hkiwooma@gmail.com>
2020-02-26 18:14:29 +0300

    modified a mark in index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const marks = require("./models/marks");
-//const student = require("./models/student");
-//class
-const sandraGrade = new marks("SST", "Sandra", 120 );
-sandraGrade.computeGrade();
 
-//const hajiGrade = new marks("Islam", "Haruna", 90);
-// hajiGrade.computeGrade();
+const sandraGrade = new marks("SST", "Sandra", 60 );
 
 module.exports = sandraGrade;
