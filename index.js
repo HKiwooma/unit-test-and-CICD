a9de6f0eea0c8c2d703f6306489caae259ce652e
HKiwooma <hkiwooma@gmail.com>
2020-02-26 18:11:27 +0300

    modified a switch statement for marks.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const marks = require("./models/marks");
 //const student = require("./models/student");
 //class
-const sandraGrade = new marks("SST", "Sandra", 60);
-// sandraGrade.computeGrade();
+const sandraGrade = new marks("SST", "Sandra", 120 );
+sandraGrade.computeGrade();
 
 //const hajiGrade = new marks("Islam", "Haruna", 90);
 // hajiGrade.computeGrade();
--- models/marks.js
+++ models/marks.js
@@ -7,30 +7,36 @@ class Marks {
   computeGrade() {
     let score = null;
     switch (true) {
-      case this.marks > 79:
-        //console.log(`${this.student} grade in ${this.subject} is A, Exellent`);
+      case this.marks > 79 && this.marks <= 100:
+        // console.log(`${this.student} grade in ${this.subject} is A, Exellent`);
         score = "A";
         break;
-      case this.marks > 69:
-        //console.log(`${this.student} grade in ${this.subject} is B, V.Good`);
+      case this.marks > 69 && this.marks < 80:
+        // console.log(`${this.student} grade in ${this.subject} is B, V.Good`);
         score = "B";
         break;
-      case this.marks > 59:
+      case this.marks > 59 && this.marks < 70:
         //console.log(`${this.student} grade in ${this.subject} is C, Good`);
         score = "C";
         break;
-      case this.marks > 49:
+      case this.marks > 49 && this.marks < 50:
         /* console.log(
           `${this.student} grade in ${this.subject} is D, Fairly passed`
         ); */
         score = "D";
         break;
-
-      default:
+      case this.marks >= 0 && this.marks < 50:
         /* console.log(
           `${this.student} grade in ${this.subject} is F, Failed \n\t\t Read Harder`
         ); */
         score = "E";
+        break;
+
+      default:
+        // console.log("OUT OF RANGE");
+        score = console.log(
+          `${this.student} grade in ${this.subject} is F, Failed \n\t\t Read Harder`
+        );null;
         break;
     }
     return score;
